8c22101f37bb39e788b9a901afd5b45b4e341385
Brian Partridge <brianpartridge@gmail.com>
2013-04-14 16:50:36 -0400

    add support for filtering the jobs list by a term
    - Ex: filter out jobs with "Static Analysis" in their name.
    - Serve icons from local server not from merowing.info.

--- jenkins_jobs_to_statusboard.rb
+++ jenkins_jobs_to_statusboard.rb
@@ -22,18 +22,31 @@ def job_order(client, job)
   end
 end
 
-throw "Pass jenkins_ip, jenkins_port, username, and password as params" unless ARGV.length >= 4
+throw "Pass jenkins_ip, jenkins_port, username, and apiToken as params. You may also include an optional term to filter out." unless ARGV.length >= 4
 
-client = JenkinsApi::Client.new(:server_ip => ARGV[0], :server_port => ARGV[1],
-                                :username => ARGV[2], :password => ARGV[3])
+address = ARGV[0]
+port = ARGV[1]
+username = ARGV[2]
+apiToken = ARGV[3]
+outputPath = ARGV[4]
+filterTerm = ARGV[5]
 
-file = File.new(ARGV[4] || "jenkins.html", "w+")
+client = JenkinsApi::Client.new(:server_ip => address, :server_port => port,
+                                :username => username, :password => apiToken)
+
+file = File.new(outputPath || "jenkins.html", "w+")
 file.puts "<table>"
 
-client.job.list_all.sort { |x, y| job_order(client, x) <=> job_order(client, y) }.each do |job|
+jobs = client.job.list_all
+if filterTerm
+    jobs = jobs.delete_if { |x| x.include? filterTerm }
+end
+jobs = jobs.sort { |x, y| job_order(client, x) <=> job_order(client, y) }
+jobs.each do |job|
   status = client.job.get_current_build_status(job)
-  file.puts "<tr><td width='50'><img width='32' height='32' src='http://www.merowing.info/wp-content/uploads/jenkins/#{status}@2x.png'/></td><td>#{job}</td></tr>"
+  file.puts "<tr><td width='50'><img width='32' height='32' src='http://#{address}/JenkinsStatusBoardIcons/#{status}@2x.png'/></td><td>#{job}</td></tr>"
 end
 file.puts "</table>"
 
 file.close()
+
